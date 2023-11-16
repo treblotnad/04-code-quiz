45717f9f617d016e3c06791b49720e41b67c217b
treblot <treblotnad@gmail.com>
2023-11-15 19:25:53 -0600

    added first event listener for start button

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var startButton = document.querySelector('#start');
+var headTitle = document.querySelector('#headTitle');
+
+startButton.addEventListener('click',function(){
+    headTitle.textContent='Working?';
+}
+)
